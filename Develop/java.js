09667949a58a749f4c51193ded99b5e6ab63854b
schevet <scheveuni@gmail.com>
2019-10-16 20:43:21 -0500

    tried adding funtion to the moment.js and did a little pseudocoding

---
+++ Develop/java.js
@@ -0,0 +1,19 @@
+// setting moment to now
+var now = moment();
+// displaying current
+$("#currentDay").text(now.format("LLLL");
+
+$(".confirm").on("click" function());
+
+var event = $(".description").trim();
+
+function(){
+    localStorage.setItem("name", event);
+}
+
+// i want to add an descrition to the event
+
+// i want the confirm button to save hte event
+
+// i want the current time to be in red, 
+// the past time in gray, and future time in green
